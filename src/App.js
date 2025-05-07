7e63135a4534b6af1895d3ffe96c1e2713b858dc
filipivanovic <filip_ivanovic@hotmail.com>
2025-05-07 16:06:14 +0200

    Add dynamic item management and form handling
    Introduced `useState` for dynamic item state in the app. Enhanced the `Form` component to handle item addition with validation, and modified `PackingList` to display the dynamically managed items.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 const initialItems = [
   {
     id: 1,
@@ -12,7 +14,7 @@ const initialItems = [
     packed: false
   },
   {
-    id: 2,
+    id: 3,
     description: 'Charger',
     quantity: 1,
     packed: true
@@ -20,11 +22,17 @@ const initialItems = [
 ]
 
 const App = () => {
+  const [items, setItems] = useState([])
+
+  const handleAddItems = item => {
+    setItems(items => [...items, item])
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   )
@@ -34,26 +42,49 @@ const Logo = () => {
   return <h1>🌴 Far Away ✈️</h1>
 }
 
-const Form = () => {
+const Form = ({ onAddItems }) => {
+  const [description, setDescription] = useState('')
+  const [quantity, setQuantity] = useState(1)
+
+  const newItem = {
+    id: Date.now(),
+    description,
+    quantity,
+    packed: false
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    if (!description) return
+    console.log(newItem)
+    setDescription('')
+    setQuantity(1)
+    onAddItems(newItem)
+  }
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip ?</h3>
-      <select>
+      <select value={quantity} onChange={e => setQuantity(Number(e.target.value))}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map(num => (
           <option key={num}>{num}</option>
         ))}
       </select>
-      <input type="text" placeholder="Item description" />
+      <input
+        type="text"
+        value={description}
+        onChange={e => setDescription(e.target.value)}
+        placeholder="Item description"
+      />
       <button>Add</button>
     </form>
   )
 }
 
-const PackingList = () => {
+const PackingList = ({ items }) => {
   return (
     <div className={`list`}>
       <ul>
-        {initialItems.map(item => (
+        {items.map(item => (
           <Item key={item.id} item={item} />
         ))}
       </ul>
